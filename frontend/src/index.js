6a4162bcbe287955134ba8efaff99c1493772bff
NoZ915 <tina0915tw0915@gmail.com>
2024-02-18 23:01:47 +0800

    finished: ~L9

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Router,
+  RouterProvider
+} from "react-router-dom";
+
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+//-----第一種寫法-----//
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomeScreen />,
+        index: true
+        //index為true代表當進入根路由"/"時，如果沒有其他子路由匹配，則顯示 <HomeScreen /> 元件
+      }
+    ]
+  }
+])
+
+//-----第二種寫法-----//
+// const router = createBrowserRouter(
+//   <Router path="/" element={<App />}>
+//     <Router index={true} paht="/" element={<HomeScreen />} />
+//   </Router>
+// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

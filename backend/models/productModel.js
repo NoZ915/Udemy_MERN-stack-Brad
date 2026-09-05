56a17b39b5d8ea58a1f06a375d49a719084f04d0
NoZ915 <tina0915tw0915@gmail.com>
2026-09-05 20:51:50 +0800

    some old changes

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -52,7 +52,7 @@ const productSchema = new mongoose.Schema({
   },
   reviews: [reviewSchema],
   rating: {
-    type: Name,
+    type: Number,
     required: true,
     default: 0
   },
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,10 @@ import dotenv from "dotenv";
 dotenv.config()
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 //後端資料夾中，from哪個檔案，必須把".js"寫上
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -14,13 +17,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-})
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-})
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server Running on port ${port}`))
